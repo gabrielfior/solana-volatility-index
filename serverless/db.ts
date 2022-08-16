8089c7abb626887a152c42864266bee8c03e68d9
DeinJoni <jonas.a.heinz@gmail.com>
2022-08-16 16:31:03 +0200

    pls help debug

--- serverless/db.ts
+++ serverless/db.ts
@@ -1,20 +1,21 @@
-import pgPromise, { IDatabase } from 'pg-promise'; // pg-promise core library
-import { IMain } from 'pg-promise';
-import { Option } from './main';
+import pgPromise, { IDatabase } from "pg-promise"; // pg-promise core library
+import { IMain } from "pg-promise";
+import { Option } from "./main";
 import * as dotenv from "dotenv";
-import { IClient } from 'pg-promise/typescript/pg-subset';
-dotenv.config({ path: __dirname + '/.env' });
+import { IClient } from "pg-promise/typescript/pg-subset";
+dotenv.config({ path: __dirname + "/.env" });
 
 export function buildDb() {
-    const pgp: IMain = pgPromise();
-    let dbConfig = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432`;
-    const db = pgp(dbConfig);
-    return db;
+  const pgp: IMain = pgPromise();
+  let dbConfig = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432`;
+  const db = pgp(dbConfig);
+  return db;
 }
 
 export async function writeItemToDb(db: any, item: Option) {
-
-    await db.none(`INSERT INTO prod_serum_option(
+  await db
+    .none(
+      `INSERT INTO prod_serum_option(
         serum_market_address,
     exchange,
     interest_rate,
@@ -30,13 +31,12 @@ export async function writeItemToDb(db: any, item: Option) {
     confidence,
     price_status,
     current_datetime) VALUES($1, $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)`,
-        Object.values(item)
+      Object.values(item)
     )
-        .then(() => {
-            //console.log('finished writing');
-        })
-        .catch(error => {
-            console.log('ERROR:', error);
-        });
+    .then(() => {
+      console.log('finished writing');
+    })
+    .catch((error) => {
+      console.log("ERROR:", error);
+    });
 }
-
